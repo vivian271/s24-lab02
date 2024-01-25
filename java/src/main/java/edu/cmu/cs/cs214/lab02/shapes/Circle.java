adf4485e17338062a1d46384af071c6569a48226
Wenhan Zhou <wzhou27@bu.edu>
2024-01-25 17:48:33 -0500

    First Version of Lab2

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
-        this.radius = radius;
+        setRadius(radius);
     }
 
     public double getArea() {
         return Math.PI * radius * radius;
+
     }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,15 +1,34 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
-    
-    public Rectangle(double height, double width){
-        this.height = height;
-        this.width = width;
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
+        setHeight(height);
+        setWidth(width);
+        // this.height = height;
+        // this.width = width;
     }
 
     public double getArea() {
         return height * width;
     }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
 }
